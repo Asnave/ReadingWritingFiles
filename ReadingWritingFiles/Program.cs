54f6b554506319dc5b5cea095254b476dd3dfb9f
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-11-29 13:06:07 -0400

    Non-Hard Coding Version of reading txt file

--- ReadingWritingFiles/Program.cs
+++ ReadingWritingFiles/Program.cs
@@ -16,18 +16,27 @@ namespace ReadingWritingFiles
             // Reading Files - story.txt file
 
             string file;
-            // Apsolute (full) Path
+            // Apsolute (full) Path HARD CODING
             file = System.IO.File.ReadAllText(@"C:\Users\W0434838\Desktop\ReadingWritingFiles\Story.txt");
             Console.WriteLine(file);
             Console.ReadKey(true);
-            // relative Path
+            // making it to an array
             string[] fileLines;
             fileLines = System.IO.File.ReadAllLines(@"C:\Users\W0434838\Desktop\ReadingWritingFiles\Story.txt");
             Console.WriteLine(fileLines[0]);
             Console.WriteLine(fileLines[1]);
             Console.ReadKey(true);
 
-            // Writing Files
+            // NOT HARD CODING -
+            // DO : Make txt file and drag it in Solution Explorer under Parent with C#, box icon and title of visual studio project 
+            // THEN : Under file name within the Solution Explorer TURN ON COPY TO OUTPUT DIRECTORY then ALWAYS COPY.
+            string [] storyFile;
+            storyFile = System.IO.File.ReadAllLines(@"Story.txt");
+            Console.WriteLine(storyFile[0]);
+            Console.WriteLine(storyFile[1]);
+            Console.WriteLine(storyFile[2]);
+            Console.WriteLine(storyFile[3]);
+            Console.ReadKey(true);
 
             // Adding Files to Cisual Studio Solution (project)
         }
