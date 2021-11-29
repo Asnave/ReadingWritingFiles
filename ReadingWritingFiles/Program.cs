06a7ead03b19b49e52f3845b061e9d0aa7949a55
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-11-29 12:44:09 -0400

    Reading Story.txt File

--- ReadingWritingFiles/Program.cs
+++ ReadingWritingFiles/Program.cs
@@ -10,6 +10,26 @@ namespace ReadingWritingFiles
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+
+            // Reading Files - story.txt file
+
+            string file;
+            // Apsolute (full) Path
+            file = System.IO.File.ReadAllText(@"C:\Users\W0434838\Desktop\ReadingWritingFiles\Story.txt");
+            Console.WriteLine(file);
+            Console.ReadKey(true);
+            // relative Path
+            string[] fileLines;
+            fileLines = System.IO.File.ReadAllLines(@"C:\Users\W0434838\Desktop\ReadingWritingFiles\Story.txt");
+            Console.WriteLine(fileLines[0]);
+            Console.WriteLine(fileLines[1]);
+            Console.ReadKey(true);
+
+            // Writing Files
+
+            // Adding Files to Cisual Studio Solution (project)
         }
     }
 }
