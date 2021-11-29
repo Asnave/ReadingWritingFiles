d90ee9d098c0d19435bc6889dd76486dd9d95043
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-11-29 13:13:28 -0400

    Saving

--- ReadingWritingFiles/Program.cs
+++ ReadingWritingFiles/Program.cs
@@ -16,10 +16,11 @@ namespace ReadingWritingFiles
             // Reading Files - story.txt file
 
             string file;
-            // Apsolute (full) Path HARD CODING
+            // Apsolute (full) Path *HARD CODING*
             file = System.IO.File.ReadAllText(@"C:\Users\W0434838\Desktop\ReadingWritingFiles\Story.txt");
             Console.WriteLine(file);
             Console.ReadKey(true);
+
             // making it to an array
             string[] fileLines;
             fileLines = System.IO.File.ReadAllLines(@"C:\Users\W0434838\Desktop\ReadingWritingFiles\Story.txt");
@@ -27,7 +28,7 @@ namespace ReadingWritingFiles
             Console.WriteLine(fileLines[1]);
             Console.ReadKey(true);
 
-            // NOT HARD CODING -
+            // *NOT HARD CODING* -
             // DO : Make txt file and drag it in Solution Explorer under Parent with C#, box icon and title of visual studio project 
             // THEN : Under file name within the Solution Explorer TURN ON COPY TO OUTPUT DIRECTORY then ALWAYS COPY.
             string [] storyFile;
@@ -38,7 +39,11 @@ namespace ReadingWritingFiles
             Console.WriteLine(storyFile[3]);
             Console.ReadKey(true);
 
-            // Adding Files to Cisual Studio Solution (project)
+            // Writing lines - SAVING 
+            System.IO.File.WriteAllLines(@"savefile.txt", storyFile);
+            Console.ReadKey(true);
+
+            
         }
     }
 }
